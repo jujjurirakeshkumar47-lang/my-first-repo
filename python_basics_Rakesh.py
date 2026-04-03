4f126fac29e88a73503d9b9e725ffd9586b15fb3
Rakesh <jujjurirakeshkumar47@gmail.com>
2026-04-03 14:12:52 +0530

    Added python_basics_Rakesh file

---
+++ python_basics_Rakesh.py
@@ -0,0 +1,20 @@
+name = "rakesh"
+age = 21
+height = 5.7
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# Age calculations
+Age_in_months = 21 *12
+Age_in_days = 21*365
+reminder = 21% 7
+age_square = 21 ** 2
+
+print("Age in months:",Age_in_months)
+print("Age in months:",Age_in_days)
+print("reminder:",reminder)
+print("age_square", age_square)
